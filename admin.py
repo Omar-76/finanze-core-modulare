7e29d1d8484040573723c26ff747a7534457d9b8
Omar-76 <omarcorio@libero.it>
2026-06-04 06:07:55 +0200

    Remove debug statement in show_admin_page function
    Removed debug print statement from show_admin_page.

--- admin.py
+++ admin.py
@@ -3,7 +3,6 @@ import layout
 
 
 def show_admin_page(supabase):
-    st.write("DEBUG: show_admin_page chiamata")
     layout.page_title("Pannello Amministratore")
     layout.page_container_start()
 
